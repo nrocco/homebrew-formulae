14510752e157fcf3f9db234cdcd750e3a9143f6c
Nico Di Rocco <dirocco.nico@gmail.com>
2015-12-09 22:37:36 +0100

    Tag correct version of consume

--- Formula/consume.rb
+++ Formula/consume.rb
@@ -3,7 +3,7 @@ class Consume < Formula
   homepage "http://nrocco.github.io"
   url "https://github.com/nrocco/consume/archive/1.1.3.tar.gz"
   version "1.1.3"
-  sha256 "ff7ea7c73ab18382ed6b052122f0918c60855422a5be320b95170ffcfcd6cbd2"
+  sha256 "de02de366f18c4998fb0ef2b1407d3d9cdfacc899dd802afa97d6fb70bbe6df1"
 
   def install
     system "make", "install", "prefix=#{prefix}"
