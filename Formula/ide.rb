65c43aba6eee96aec10f93a660f36b0b804daa75
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2016-06-19 09:08:26 -0700

    Added new Formula ide

---
+++ Formula/ide.rb
@@ -0,0 +1,13 @@
+class Ide < Formula
+  desc "ide provides a powerful ide that gets out of your way"
+  homepage "http://nrocco.github.io"
+  head "https://github.com/nrocco/ide.git"
+
+  # def install
+  #   system "make", "install", "prefix=#{prefix}"
+  # end
+
+  test do
+    system "false"
+  end
+end
