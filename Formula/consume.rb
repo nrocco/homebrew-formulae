f66737855c2da038c2857ad83358ae4e3f54a8d4
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2016-07-02 19:24:03 +0200

    Update consume to 1.2.0

--- Formula/consume.rb
+++ Formula/consume.rb
@@ -1,9 +1,9 @@
 class Consume < Formula
   desc "wrapper around HTTPie for friendly rest api consumption"
   homepage "http://nrocco.github.io"
-  url "https://github.com/nrocco/consume/archive/1.1.3.tar.gz"
-  version "1.1.3"
-  sha256 "de02de366f18c4998fb0ef2b1407d3d9cdfacc899dd802afa97d6fb70bbe6df1"
+  url "https://github.com/nrocco/consume/archive/1.2.0.tar.gz"
+  version "1.2.0"
+  sha256 "bbf9a7a52b13aee55ada1ba4421b145733d88b883f9e1a67a29e0bc62720f2d9"
 
   def install
     system "make", "install", "prefix=#{prefix}"
