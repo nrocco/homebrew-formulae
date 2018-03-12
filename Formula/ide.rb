1acba24102afaa1ed28d4885b702ae3a3949d4ce
Nico Di Rocco <dirocco.nico@gmail.com>
2018-03-12 21:33:28 +0100

    Switch from HEAD to normal fomula for ide

--- Formula/ide.rb
+++ Formula/ide.rb
@@ -1,10 +1,15 @@
 class Ide < Formula
   desc "ide provides a powerful ide that gets out of your way"
   homepage "https://github.com/nrocco/ide"
-  head "https://github.com/nrocco/ide.git"
+  url "https://github.com/nrocco/ide/releases/download/0.9.10/ide-0.9.10-darwin-amd64.tar.gz"
+
+  version "0.9.10"
+
+  sha256 "85444f181b91550d9779918c0f013bfc7850060178785e58bf8460b889e3094c"
 
   def install
-    system "make", "install", "prefix=#{prefix}"
+      bin.install "ide"
+      bin.install "rgit"
   end
 
   test do
