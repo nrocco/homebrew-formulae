d3ec3aa7f3fe0b90aafdcc2d20cfb8496e87cee8
Nico Di Rocco <dirocco.nico@gmail.com>
2018-08-12 20:08:29 +0200

    Update soap_client to version 3.0.0

--- Formula/soap_client.rb
+++ Formula/soap_client.rb
@@ -1,9 +1,9 @@
 class SoapClient < Formula
   desc "a cli tool to consume SOAP web services"
   homepage "http://nrocco.github.io"
-  url "http://leaseweb.github.io/php-soap-client/soap_client.phar"
-  version "2.1.7"
-  sha256 "a85241d8f2a106e00be90c8d201be2a5dac3a567cd14809733e961a660c69f17"
+  url "https://github.com/LeaseWeb/php-soap-client/releases/download/3.0.0/soap_client.phar"
+  version "3.0.0"
+  sha256 "bc373d8fab71914c2452f198b983ede03c362cd86cd1d6095c641330a27aedd9"
 
   def install
     system "mv", "soap_client.phar", "soap_client"
